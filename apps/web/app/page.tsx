426af627ccdfc4c8749e6abda8af8c49aca95104
Vatsal Umrania <vbumrania@gmail.com>
2025-09-16 22:10:09 +0530

    Revert "02 : convex package"

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,15 @@
-"use client"
-
-import {useQuery} from "convex/react";
-import {api} from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button"
+import  {add} from "@workspace/math/add"
+import {Input} from "@workspace/ui/components/input"
 export default function Page() {
-  const users = useQuery(api.user.getMany, {});
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <p>apps/web</p>
-      <div className="max-w-sm p-4 ">
-        {JSON.stringify(users)}
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Hello Web</h1>
+        <Button size="sm">Button</Button>
+        <p className="text-lg">2 + 3 = {add(2, 3)}</p>
+        <Input/>
       </div>
-      
     </div>
   )
- }
+}
--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -1,13 +1,18 @@
 "use client"
 
 import * as React from "react"
-import { ConvexProvider, ConvexReactClient } from "convex/react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "")
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ConvexProvider client={convex}>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      enableColorScheme
+    >
       {children}
-    </ConvexProvider>
+    </NextThemesProvider>
   )
 }
