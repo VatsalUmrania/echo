7174a961a66efde993b069826c9388de7c6a6a28
Vatsal Umrania <vbumrania@gmail.com>
2025-09-16 20:55:34 +0530

    02 : convex package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,16 @@
-import { Button } from "@workspace/ui/components/button"
-import  {add} from "@workspace/math/add"
-import {Input} from "@workspace/ui/components/input"
+"use client"
+
+import {useQuery} from "convex/react";
+import {api} from "@workspace/backend/_generated/api";
 export default function Page() {
+  const users = useQuery(api.user.getMany, {});
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello Web</h1>
-        <Button size="sm">Button</Button>
-        <p className="text-lg">2 + 3 = {add(2, 3)}</p>
-        <Input/>
+      <p>apps/web</p>
+      <div className="max-w-sm p-4 ">
+        {JSON.stringify(users)}
       </div>
+      
     </div>
   )
-}
+ }
